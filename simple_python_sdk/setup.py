ba18145c82ac8946c0e10ae85504a3646b9c302f
saroeun.ron <saroeun.ron@wingbank.com.kh>
2025-05-02 15:58:24 +0700

    asdfgh

--- simple_python_sdk/setup.py
+++ simple_python_sdk/setup.py
@@ -6,11 +6,11 @@ with open("README.md","r") as file:
 
 setup(
     name="grpc_orchestrator",
-    version="0.0.2",
+    version="0.0.3",
     author="Ron Saroeun",
     author_email="ronsaroeun668@gmail.com",
     url="https://github.com/bunrongGithub/grpc_orchestrator/tree/backup/simple_python_sdk",
     long_description=description,
     long_description_content_type="text/markdown"
-    
+
 )
