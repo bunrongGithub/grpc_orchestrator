d6941ffdd4c16e98d61ebacfd6ed46bca678ac8a
saroeun.ron <saroeun.ron@wingbank.com.kh>
2025-05-13 10:58:37 +0700

    add metadata

--- simple_python_sdk/setup.py
+++ simple_python_sdk/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="grpc_orchestrator",
-    version="0.0.5",
+    version="0.0.6",
     author="Ron Saroeun",
     author_email="ronsaroeun668@gmail.com",
     url="https://github.com/bunrongGithub/grpc_orchestrator/tree/backup/simple_python_sdk",
@@ -13,15 +13,32 @@ setup(
     long_description_content_type="text/markdown",
     description="A gRPC-based Saga Orchestration System for distributed transactions",
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+   classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: System :: Distributed Computing",
+    "Framework :: AsyncIO",
+],
+
     python_requires=">=3.7",
     install_requires=[
         "grpcio>=1.56",
         "protobuf>=4.21"
+    ],
+    keywords=[
+        "grpc",
+        "grpc orchestrator",
+        "grpc microservices",
+        "distributed transactions",
+        "python grpc",
+        "saga pattern",
+        "grpc saga",
+        "transaction orchestrator",
+        "python microservices",
     ],
     include_package_data=True,
 )
